ed5915de4458210c20f60d4490fdf257be2cf1c1
Aviwe Gift Ndalana <Aviegift@gmail.com>
2019-05-09 13:06:58 +0200

    Completed the interface theme and other issues

--- onlineshopping/src/main/java/net/avie/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/avie/onlineshopping/controller/PageController.java
@@ -11,7 +11,8 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Avie Web MVC");
+		mv.addObject("tittle", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,12 @@
+$(function() {
+	//solving the active menu problem
+	switch(menu) {
+	
+		case 'Home':
+			$('#home').addClass('active');
+			break;
+	}
+	
+	
+	
+});
